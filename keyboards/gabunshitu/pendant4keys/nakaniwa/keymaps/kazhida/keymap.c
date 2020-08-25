69dd568afeadf81f2e29046c2e4e1914bfec3b7b
kazhida <kazhida@abplus.com>
2020-08-25 10:10:39 +0900

    左手にエンターとエスケープを追加

--- keyboards/gabunshitu/pendant4keys/nakaniwa/keymaps/kazhida/keymap.c
+++ keyboards/gabunshitu/pendant4keys/nakaniwa/keymaps/kazhida/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,           KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC,
         KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT,
         KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,           KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,KC_UP,  KC_DEL,
-        KC_NO,  KC_LALT,KC_LGUI,KC_LALT,KL_EISU,                KS_KANA,KC_SPC,                 KL_KANA,KC_LEFT,KC_DOWN,KC_RIGHT
+        KC_ENT, KC_LALT,KC_LGUI,KC_LALT,KL_EISU,                KS_KANA,KC_SPC,                 KL_KANA,KC_LEFT,KC_DOWN,KC_RIGHT
     ),
     [_LOWER] = LAYOUT(
         KC_1,   KC_2,   KC_3,   KC_4,
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,LGUI(KC_Q), LGUI(KC_W), LGUI(KC_E), LGUI(KC_R), LGUI(KC_T),     LGUI(KC_Y), LGUI(KC_U), LGUI(KC_I), LGUI(KC_O), LGUI(KC_P), _______,_______,LGUI(KC_BSPC),
         _______,LGUI(KC_A), LGUI(KC_S), LGUI(KC_D), LGUI(KC_F), LGUI(KC_G),     LGUI(KC_H), LGUI(KC_J), LGUI(KC_K), LGUI(KC_L), _______,    _______,LGUI(KC_ENT),
         _______,LGUI(KC_Z), LGUI(KC_X), LGUI(KC_C), LGUI(KC_V), LGUI(KC_B),     LGUI(KC_N), LGUI(KC_M), LGUI(KC_M), LGUI(KC_COMM),LGUI(KC_SLSH),    _______,KC_PGUP,_______,
-        _______,_______,_______,_______,_______,                                KC_SPC,     _______,                                _______,    KC_HOME,KC_PGDN, KC_END
+        KC_ESC, _______,_______,_______,_______,                                KC_SPC,     _______,                                _______,    KC_HOME,KC_PGDN, KC_END
 //        KC_1,   KC_2,   KC_3,   KC_4,
 //        KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,          KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, _______,_______,
 //        _______,KC_F5,  KC_F6,  KC_F7,  KC_F8,  _______,        _______,_______,_______,_______,_______,_______,_______,_______,
