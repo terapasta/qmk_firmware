c844f9e976a0533edf10676a9ac62953ec80dca2
kazhida <kazhida@abplus.com>
2020-08-25 10:13:12 +0900

    デリートも追加した

--- keyboards/gabunshitu/pendant4keys/nakaniwa/keymaps/kazhida/keymap.c
+++ keyboards/gabunshitu/pendant4keys/nakaniwa/keymaps/kazhida/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,RSFT(KC_Q), RSFT(KC_W), RSFT(KC_E), RSFT(KC_R), RSFT(KC_T),     RSFT(KC_Y), RSFT(KC_U), RSFT(KC_I), RSFT(KC_O), RSFT(KC_P), KC_LCBR,KC_RCBR,RSFT(KC_BSPC),
         _______,RSFT(KC_A), RSFT(KC_S), RSFT(KC_D), RSFT(KC_F), RSFT(KC_G),     RSFT(KC_H), RSFT(KC_J), RSFT(KC_K), RSFT(KC_L), KC_COLN,    KC_DQUO,RSFT(KC_ENT),
         _______,RSFT(KC_Z), RSFT(KC_X), RSFT(KC_C), RSFT(KC_V), RSFT(KC_B),     RSFT(KC_N), RSFT(KC_M), KC_LABK,    KC_RABK,    KC_QUES,    _______,KC_PGUP,_______,
-        _______,_______,_______,_______,_______,                                _______,    _______,                                _______,KC_HOME,KC_PGDN,KC_END
+        KC_DEL, _______,_______,_______,_______,                                _______,    _______,                                _______,KC_HOME,KC_PGDN,KC_END
 //        KC_1,   KC_2,   KC_3,   KC_4,
 //        KC_TILD,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,          KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_UNDS,KC_PLUS,KC_TILD,KC_PIPE,
 //        _______,KC_F5,  KC_F6,  KC_F7,  KC_F8,  _______,        _______,_______,_______,_______,_______,_______,_______,_______,
