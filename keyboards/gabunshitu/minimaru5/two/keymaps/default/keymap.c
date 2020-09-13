099b0b32c5f100c339a6c613e2eafbe05d98be68
kazhida <kazhida@abplus.com>
2020-09-13 19:50:44 +0900

    minimaru5/twoの実装

--- keyboards/gabunshitu/minimaru5/two/keymaps/default/keymap.c
+++ keyboards/gabunshitu/minimaru5/two/keymaps/default/keymap.c
@@ -30,20 +30,19 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [_BASE] = LAYOUT(
-                KC_1,   KC_2,   KC_3,
-                KC_4,   KC_5,   KC_6,
-                KC_7,   KC_8,   KC_9,
-                KC_0,   KC_A,   KC_B,
-        KC_C,   KC_D,   KC_E,   KC_F
+                KC_1,   KC_2,   KC_3,   KC_G,   KC_H,   KC_I,
+                KC_4,   KC_5,   KC_6,   KC_J,   KC_K,   KC_L,
+                KC_7,   KC_8,   KC_9,   KC_M,   KC_N,   KC_O,
+                KC_0,   KC_A,   KC_B,   KC_P,   KC_Q,   KC_R,
+        KC_C,   KC_D,   KC_E,   KC_F,   KC_S,   KC_T,   KC_U
     ),
     [_FN] = LAYOUT(
-                _______,_______,_______,
-                _______,_______,_______,
-                _______,_______,_______,
-                _______,_______,_______,
-        _______,_______,_______,_______
+                _______,_______,_______,_______,_______,_______,
+                _______,_______,_______,_______,_______,_______,
+                _______,_______,_______,_______,_______,_______,
+                _______,_______,_______,_______,_______,_______,
+        _______,_______,_______,_______,_______,_______,_______
     )
-
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
