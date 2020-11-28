ba83bc64b528dca5c233b1928e32ee5b1a5bc227
kazhida <kazhida@abplus.com>
2020-11-28 17:37:14 +0900

    nakaniwaのlowerにScrollLoxkなどを割り付け

--- keyboards/gabunshitu/nakaniwa/keymaps/kazhida/keymap.c
+++ keyboards/gabunshitu/nakaniwa/keymaps/kazhida/keymap.c
@@ -11,13 +11,14 @@ enum layer_number {
 #define KL_KANA LT(_RAISE, KC_LANG1)
 #define cmd(kc) LGUI(kc)
 #define sft(kc) RSFT(kc)
+#define RC_F3 RSFT(KC_F3)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-            KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_FN1,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV,
+            KC_ESC, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   RC_F3,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL, KC_BSLS,KC_GRV,
             KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC,KC_RBRC,KC_BSPC,
             KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,KC_ENT,
-            KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_FN2, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,KC_UP,  KC_DEL,
+            KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_F3,  KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_RSFT,KC_UP,  KC_DEL,
             KC_LCTL,KC_LALT,KC_LALT,KC_LGUI,KL_EISU,                    KL_KANA,KC_SPC, KC_SPC, KC_SPC,         KC_RSFT,KC_LEFT,KC_DOWN,KC_RIGHT
     ),
     [_LOWER] = LAYOUT(
--- keyboards/gabunshitu/nakaniwa/keymaps/like_jis/keymap.c
+++ keyboards/gabunshitu/nakaniwa/keymaps/like_jis/keymap.c
@@ -24,25 +24,24 @@ enum layer_number {
 
 #define KL_EISU LT(_LOWER, KC_LANG2)
 #define KL_KANA LT(_RAISE, KC_LANG1)
-#define KC_FN1  KC_SPC
-#define KC_FN2  KC_SPC
 #define cmd(kc) LCTL(kc)
 #define sft(kc) RSFT(kc)
+#define RC_F3 RSFT(KC_F3)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
-            KC_ESC,     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_FN1,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   JP_MINS,JP_CIRC, JP_YEN,KC_BSPC,
+            KC_ESC,     KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   RC_F3,      KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   JP_MINS,JP_CIRC, JP_YEN,KC_BSPC,
             KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   JP_AT,JP_RBRC,JP_LBRC,
             KC_LCTL,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   JP_SCLN,JP_COLN,KC_ENT,
-            KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_FN2, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,JP_BSLS,KC_UP,  KC_RSFT,
-            KC_LCTRL,   KC_LALT,KC_LALT,KC_LGUI,KL_EISU,                    KL_KANA,KC_SPC, KC_SPC, KC_SPC,         KL_KANA,KC_LEFT,KC_DOWN,KC_RIGHT
+            KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_F3,  KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,JP_BSLS,KC_UP,  KC_RSFT,
+            KC_LCTRL,   KC_LALT,KC_LALT,KC_LGUI,KL_EISU,                    KL_KANA,KC_SPC, KC_SPC, KC_SPC,         KC_RSFT,KC_LEFT,KC_DOWN,KC_RIGHT
     ),
     [_LOWER] = LAYOUT(
             KC_GRV, KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      _______,    KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11, KC_F12, _______,_______,
-            _______,cmd(KC_Q), cmd(KC_W), cmd(KC_E), cmd(KC_R), cmd(KC_T),             cmd(KC_Y), cmd(KC_U), cmd(KC_I), cmd(KC_O), cmd(KC_P), _______,_______,cmd(KC_BSPC),
-            _______,cmd(KC_A), cmd(KC_S), cmd(KC_D), cmd(KC_F), cmd(KC_G),             cmd(KC_H), cmd(KC_J), cmd(KC_K), cmd(KC_L), _______,    _______,cmd(KC_ENT),
-            _______,cmd(KC_Z), cmd(KC_X), cmd(KC_C), cmd(KC_V), cmd(KC_B),     _______,cmd(KC_N), cmd(KC_M), cmd(KC_M), cmd(KC_COMM),cmd(KC_SLSH),    _______,KC_PGUP,_______,
-            _______,_______,_______,_______,_______,                                        _______,    _______,    _______,    _______,            _______,    KC_HOME,KC_PGDN, KC_END
+            _______,cmd(KC_Q), cmd(KC_W), cmd(KC_E), cmd(KC_R), cmd(KC_T),             cmd(KC_Y), cmd(KC_U), KC_PSCR, KC_SLCK, KC_PAUS, _______,_______,cmd(KC_BSPC),
+            _______,cmd(KC_A), cmd(KC_S), cmd(KC_D), cmd(KC_F), cmd(KC_G),             cmd(KC_H), KC_INS,   KC_HOME,  KC_PGUP, _______,_______,cmd(KC_ENT),
+            _______,cmd(KC_Z), cmd(KC_X), cmd(KC_C), cmd(KC_V), cmd(KC_B),     _______,cmd(KC_N), KC_DEL,   KC_END,   KC_PGDN,cmd(KC_SLSH),_______,KC_PGUP,KC_DEL,
+            _______,_______,_______,_______,_______,                           _______,    _______,    _______,    _______,            _______,    KC_HOME,KC_PGDN, KC_END
     ),
     [_RAISE] = LAYOUT(
             KC_TILD,KC_EXLM,    KC_AT,      KC_HASH,    KC_DLR,     KC_PERC,    _______,    KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_UNDS, KC_PLUS,KC_PIPE,KC_TILD,
