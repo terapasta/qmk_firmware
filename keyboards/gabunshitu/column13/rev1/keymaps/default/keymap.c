a60af1c8dd7ae10895d8fa7c2b2cc731b301b3c7
kazhida <kazhida@abplus.com>
2021-01-25 12:40:41 +0900

    キーマップを調整

--- keyboards/gabunshitu/column13/rev1/keymaps/default/keymap.c
+++ keyboards/gabunshitu/column13/rev1/keymaps/default/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, KC_BSPC,
     KL_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_GRV,  KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
-    KC_LCTL, KC_LGUI, KC_DEL,  KA_SPC,  KL_EISU,         KS_KANA, KL_SPC,  KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL, KC_LGUI, KC_DEL,  KA_SPC,  KL_EISU,         KS_KANA, KL_SPC,  KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Lower
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  cmd(KC_Q), cmd(KC_W), cmd(KC_E), cmd(KC_R), cmd(KC_T), cmd(KC_Y), cmd(KC_U), cmd(KC_I), cmd(KC_O),  cmd(KC_P),KC_EQL,_______,
     _______, cmd(KC_A), cmd(KC_S), cmd(KC_D), cmd(KC_F), cmd(KC_G), cmd(KC_H), cmd(KC_J), cmd(KC_K), cmd(KC_L),  KC_COLN, _______,
     _______, cmd(KC_Z), cmd(KC_X), cmd(KC_C), cmd(KC_V), cmd(KC_B), cmd(KC_N), cmd(KC_M), KC_LCBR,   KC_RCBR,    KC_PGUP, KC_BSLS,
-    _______, _______,   _______,   _______,   _______,              _______,   _______,   _______,   KC_HOME,    KC_PGDN, KC_END
+    _______, _______,   _______,   _______,   _______,              _______,   _______,   KC_BSLS,   KC_HOME,    KC_PGDN, KC_END
 ),
 
 /* Raise
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL,  _______,
     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_SCLN,_______,
     _______,_______,_______,_______,_______,_______,_______,_______,KC_LBRC,KC_RBRC,KC_PGUP,KC_BSLS,
-    _______,_______,_______,_______,_______,        _______,_______,_______,KC_HOME,KC_PGDN,KC_END
+    _______,_______,_______,_______,_______,        _______,_______,KC_BSLS,KC_HOME,KC_PGDN,KC_END
 ),
 [_FUNCS] =  LAYOUT(
     _______,KC_1,   KC_2,   KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12,
