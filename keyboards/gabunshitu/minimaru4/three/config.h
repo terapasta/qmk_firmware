f8a9fecc6f9a7e0844cec1ae59569b3be55bb5b3
kazhida <kazhida@abplus.com>
2021-01-31 16:53:09 +0900

    全部繋げたfull版を追加

--- keyboards/gabunshitu/minimaru4/three/config.h
+++ keyboards/gabunshitu/minimaru4/three/config.h
@@ -51,21 +51,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
-#define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
+//#define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
 
-//#define BACKLIGHT_PIN B5
+//#define BACKLIGHT_PIN D3
 //#define BACKLIGHT_BREATHING
 //#define BACKLIGHT_LEVELS 3
+//#define DRIVER_LED_TOTAL 48
 
-#define RGB_DI_PIN B5
+#define RGB_DI_PIN D3
  #ifdef RGB_DI_PIN
    #define RGBLED_NUM 48
    #define DRIVER_LED_TOTAL RGBLED_NUM
    #define RGBLIGHT_HUE_STEP 8
    #define RGBLIGHT_SAT_STEP 8
    #define RGBLIGHT_VAL_STEP 8
-   #define RGBLIGHT_LIMIT_VAL 150 /* The maximum brightness level */
-   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+   #define RGBLIGHT_LIMIT_VAL 64 /* The maximum brightness level */
+//   #define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
 // /*== all animations enable ==*/
 //   #define RGBLIGHT_ANIMATIONS
 // /*== or choose animations ==*/
@@ -76,7 +77,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //   #define RGBLIGHT_EFFECT_KNIGHT
 //   #define RGBLIGHT_EFFECT_CHRISTMAS
 //   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-//   #define RGBLIGHT_EFFECT_RGB_TEST
+   #define RGBLIGHT_EFFECT_RGB_TEST
 //   #define RGBLIGHT_EFFECT_ALTERNATING
 // /*== customize breathing effect ==*/
 //   /*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
