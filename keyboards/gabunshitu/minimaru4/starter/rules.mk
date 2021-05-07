6ccbb5a69504e5fd1aac18a98c620ced059183d3
kazhida <kazhida@abplus.com>
2021-05-07 20:00:08 +0900

    何故か動かなくなっていたので慌てて修正（昔のコードにもどした）

--- keyboards/gabunshitu/minimaru4/starter/rules.mk
+++ keyboards/gabunshitu/minimaru4/starter/rules.mk
@@ -16,15 +16,15 @@ BOOTLOADER = atmel-dfu
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
+EXTRAKEY_ENABLE = yes      # Audio control and System control
+CONSOLE_ENABLE = yes       # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 MIDI_ENABLE = no            # MIDI support
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output
@@ -32,9 +32,10 @@ FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 HD44780_ENABLE = no         # Enable support for HD44780 based LCDs
 
 #RGB_MATRIX_ENABLE = no
-RGB_MATRIX_DRIVER = WS2812
+#RGB_MATRIX_DRIVER = WS2812
 
 CUSTOM_MATRIX = lite
 SRC += matrix.c
-SRC += gabunshitu/modulo/atmega32u4/i2c_master.c
 SRC += gabunshitu/modulo/atmega32u4/ti_xpa95xx/expander.c
+SRC += gabunshitu/modulo/atmega32u4/i2c_master.c
+#QUANTUM_LIB_SRC += i2c_master.c
--- keyboards/gabunshitu/modulo/atmega32u4/ti_xpa95xx/expander.c
+++ keyboards/gabunshitu/modulo/atmega32u4/ti_xpa95xx/expander.c
@@ -4,8 +4,8 @@
 
 #include <avr/interrupt.h>
 #include <wait.h>
-#include <i2c_master.h>
 #include <debug.h>
+#include <i2c_master.h>
 
 #include "expander.h"
 
@@ -29,50 +29,30 @@ void expander_init(const expander *slaves, uint8_t count) {
     // make all pins to input mode
     for (uint8_t i = 0; i < count; i++) {
         uint8_t num = slaves[i].num_port;
-        uint8_t addr = slaves[i].dev_address << 1;
-        uint8_t conf = slaves[i].cfg_address;
-        uint8_t pull = slaves[i].pup_address;
-        uint8_t data = ALL_INPUT;
+        uint8_t addr = slaves[i].address << 1;
         for (uint8_t j = 0; j < num; j++) {
-            uint8_t reg = conf + j;
-            i2c_status_t ret = i2c_writeReg(addr, reg, &data, sizeof(data), TIMEOUT);
+            uint8_t cmd = CMD_CONFIG * num + j;
+            uint8_t conf = ALL_INPUT;
+            i2c_status_t ret = i2c_writeReg(addr, cmd, &conf, sizeof(conf), TIMEOUT);
             if (ret != I2C_STATUS_SUCCESS) {
-                xprintf("config pins all-input FAILED: %02X, addr: %02X, reg: %02X data: %d\n", ret, addr, reg, data);
+                xprintf("config pins FAILED: %d, addr: %02X, cmd: %02X conf: %d\n", ret, addr, cmd, conf);
             } else {
-                xprintf("config pins all-input SUCCEEDED: %02X, addr: %02X, reg: %02X data: %d\n", ret, addr, reg, data);
-            }
-        }
-        if (conf != pull) {
-            for (uint8_t j = 0; j < num; j++) {
-                uint8_t reg = pull + j;
-                i2c_status_t ret = i2c_writeReg(addr, reg, &data, sizeof(data), TIMEOUT);
-                if (ret != I2C_STATUS_SUCCESS) {
-                    xprintf("config pins pull-up FAILED: %02X, addr: %02X, reg: %02X data: %d\n", ret, addr, reg, data);
-                } else {
-                    xprintf("config pins pull-up SUCCEEDED: %02X, addr: %02X, reg: %02X data: %d\n", ret, addr, reg, data);
-                }
+                xprintf("config pins SUCCEEDED: %d, addr: %02X, cmd: %02X conf: %d\n", ret, addr, cmd, conf);
             }
         }
     }
 }
 
 uint16_t expander_readPins(const expander *slave) {
-    uint8_t addr = slave->dev_address << 1;
+    uint8_t addr = slave->address << 1;
     uint8_t data[2] = { 0xff, 0xff };     // num_port <= 2
-    uint8_t reg = slave->reg_address;
+    uint8_t cmd = CMD_INPUT * slave->num_port;
 
-    for (uint8_t i = 0; i < slave->num_port; i++) {
-        i2c_status_t ret = i2c_readReg(addr, reg + i, data + i, 1, TIMEOUT);
-        if (ret != I2C_STATUS_SUCCESS) {
-            return 0x0000;
-        }
+    i2c_status_t ret = i2c_readReg(addr, cmd, data, slave->num_port, TIMEOUT);
+    if (ret != I2C_STATUS_SUCCESS) {
+//        xprintf("expander_readPins FAILED: %d, addr: %02X, cmd: %02X\n", ret, addr, cmd);
+        return 0x0000;
     }
-
-//    i2c_status_t ret = i2c_readReg(addr, reg, data, slave->num_port, TIMEOUT);
-//    if (ret != I2C_STATUS_SUCCESS) {
-//        xprintf("expander_readPins FAILED: %d, addr: %02X, reg: %02X\n", ret, addr, reg);
-//        return 0x0000;
-//    }
 
     uint16_t result = 0;
     for (int8_t i = slave->num_port - 1; i >= 0; i--) {
@@ -83,7 +63,7 @@ uint16_t expander_readPins(const expander *slave) {
     result = ~result;
     result &= slave->mask;
 //    if (result != 0) {
-//        xprintf("expander_readPins %04X, addr: %02X, reg: %02X\n", result, addr, reg);
+//        xprintf("expander_readPins %04X, addr: %02X, cmd: %02X\n", result, addr, cmd);
 //    }
     return result;
 }
