5b7b03b2068e874507f136144f4d70e565198bfd
kazhida <kazhida@abplus.com>
2021-11-06 10:48:16 +0900

    Remap対応用の修正

--- keyboards/gabunshitu/column13/ansi2/config.h
+++ keyboards/gabunshitu/column13/ansi2/config.h
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0xAB01
-#define PRODUCT_ID   0xC130
-#define DEVICE_VER   0x0002
+#define PRODUCT_ID   0xC131
+#define DEVICE_VER   0x0001
 #define MANUFACTURER ABplus
 #define PRODUCT      Column13-ansi2
 
