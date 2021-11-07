ca816717ca1cf070bd9a3411bc8358831cc84caf
kazhida <kazhida@abplus.com>
2021-11-07 18:52:47 +0900

    Remap対応用の修正

--- keyboards/gabunshitu/column13/iso/config.h
+++ keyboards/gabunshitu/column13/iso/config.h
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* USB Device descriptor parameter */
 #define VENDOR_ID    0xAB01
-#define PRODUCT_ID   0xC131
-#define DEVICE_VER   0x0010
+#define PRODUCT_ID   0xC135
+#define DEVICE_VER   0x0001
 #define MANUFACTURER ABplus
 #define PRODUCT      Column13-iso
 
